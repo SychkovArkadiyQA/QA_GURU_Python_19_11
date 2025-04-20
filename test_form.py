c82726af22c9769215f11b93069a8c13fa82a64a
arkadijsyckov <Z4mmer@gmail.com>
2025-04-21 01:49:37 +0500

    add conftest

--- test_form.py
+++ test_form.py
@@ -1,30 +1,5 @@
 import os
-
-import pytest
 from selene import browser, have, by
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-selenoid_capabilities = {
-    "browserName": "chrome",
-    "browserVersion": "128.0",
-    "selenoid:options": {
-        "enableVNC": True,
-        "enableVideo": False}
-}
-options.capabilities.update(selenoid_capabilities)
-driver = webdriver.Remote(
-    command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    options=options)
-
-browser.config.driver = driver
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.window_width = 1400
-    browser.config.window_height = 1200
-    browser.config.base_url = 'https://demoqa.com'
 
 
 def test_complete_form():
