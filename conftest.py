3ce258f9c749d6ff460787ab99fed514a44e6918
arkadijsyckov <Z4mmer@gmail.com>
2025-04-21 03:45:51 +0500

    fix

--- conftest.py
+++ conftest.py
@@ -3,13 +3,25 @@ import os
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 from utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
-def setup_browser(request):
-    browser.config.window_width = 1400
-    browser.config.window_height = 1200
+def browser_management():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -21,7 +33,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
