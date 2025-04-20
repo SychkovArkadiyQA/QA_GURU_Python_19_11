8356ff206258fbcb4aca146d8e8c17581436092e
arkadijsyckov <Z4mmer@gmail.com>
2025-04-21 03:16:45 +0500

    add video

--- conftest.py
+++ conftest.py
@@ -4,15 +4,6 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
-from dotenv import load_dotenv
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
@@ -25,12 +16,12 @@ def setup_browser(request):
         "browserVersion": "128.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
@@ -40,6 +31,7 @@ def setup_browser(request):
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
 
